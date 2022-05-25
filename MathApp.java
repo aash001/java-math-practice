f1e9d0f293e144a5200c1e052c974bfd1a26f88b
aash001 <ampinarioash@users.noreply.github.com>
2022-05-25 18:00:12 -0400

    Add solutions 1 - 4

--- MathApp.java
+++ MathApp.java
@@ -1,5 +1,24 @@
 public class MathApp {
   public static void main(String[] args) {
-    System.out.println("Answer goes here");
+    // Problem #1
+    int job1Salary = 28000;
+    int job2Salary = 89000;
+    System.out.println(Math.max(job1Salary, job2Salary));
+    // Problem #2
+    int carPrice = 10000;
+    int truckPrice = 48000;
+    System.out.println(Math.min(carPrice, truckPrice));
+    // Problem #3
+    double radius = 7.25;
+    double area = (radius * radius) * Math.PI;
+    System.out.println(area);
+    // Problem #4
+    double num1 = 5.0;
+    double squareRoot = Math.sqrt(num1);
+    System.out.println(squareRoot);
+    // Problem #5
+    int differenceOfP1 = 85 - 5;
+    int differenceOfP2 = 50 - 10;
+
   }
 }
